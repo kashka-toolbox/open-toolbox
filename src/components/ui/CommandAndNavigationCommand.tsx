021295fa2d57cf17e5b289f55f1039d91b0be15a
Morten Renner <github@mortenrenner.de>
2024-03-28 03:28:49 +0100

    add: description on hover to CMD-Nav

--- src/components/ui/CommandAndNavigationCommand.tsx
+++ src/components/ui/CommandAndNavigationCommand.tsx
@@ -29,11 +29,11 @@ export function CommandAndNavigationCommand() {
                     if (subItem.href) router.push(subItem.href);
                   }}
                   key={subIndex}
-                  className="hover:cursor-pointer flex flex-row items-baseline overflow-hidden text-nowrap"
+                  className="hover:cursor-pointer flex flex-row items-baseline overflow-hidden text-nowrap group/cmd"
                 >
                   {subItem.icon ? <subItem.icon className="mr-2 h-4 w-4 min-w-4 place-self-center" /> : <div className="mr-2 h-4 w-4 min-w-4" />}
-                  <span className="text-nowrap">{subItem.title}</span>
-                  <span className="text-muted-foreground pl-2 text-xs text-ellipsis overflow-hidden min-w-0">{subItem.description}</span>
+                  <span className="text-nowrap inline-block">{subItem.title}</span>
+                  <span className="text-muted-foreground pl-2 text-xs text-ellipsis overflow-hidden min-w-0 transition-opacity duration-100 opacity-0 group-data-[selected='true']/cmd:opacity-100">{subItem.description}</span>
                   <CommandShortcut className="w-fit pl-2">{subItem.shortcut}</CommandShortcut>
                 </CommandItem>
               ))}
