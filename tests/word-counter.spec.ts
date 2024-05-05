5871869edd5fffe54ee34c5ed878171adad33a30
Morten Renner <github@mortenrenner.de>
2024-05-06 00:24:02 +0200

    fix: playwright url match issue caused by i18n

--- tests/word-counter.spec.ts
+++ tests/word-counter.spec.ts
@@ -7,7 +7,7 @@ test('should be able to navigate to the word counter and count some characters',
   await page.goto('./')
   await page.getByPlaceholder('Type a command or search...').fill("Word Counter")
   await page.click('text=Word Counter')
-  await expect(page).toHaveURL('./tools/word-counter')
+  await expect(page).toHaveURL(/\.\/*\/tools\/word-counter/)
   await expect(page.locator('span', {hasText: "Word Counter"})).toBeVisible()
   await page.getByLabel('Enter your Text here:').fill("Hello World!")
   await expect(page.getByText('12')).toBeInViewport()
