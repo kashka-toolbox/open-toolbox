863727b3b7c509db28c93dfbe33449985d860714
Morten Renner <github@mortenrenner.de>
2024-08-05 20:51:38 +0200

    add: Status Indicator

---
+++ src/components/ui/status-indicator.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import { VariantProps, cva } from "class-variance-authority"
+import { CheckIcon } from "@radix-ui/react-icons"
+
+const alertVariants = cva(
+  "relative",
+  {
+    variants: {
+      variant: {
+        default: "bg-background text-foreground",
+        destructive:
+          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+const possibleStatuses = ["unchecked", "checked", "progress"] as const;
+type Status = typeof possibleStatuses[number];
+
+const StatusIndicator = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants> & {
+    status: Status
+  }
+>(({ className, variant, status, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(alertVariants({ variant }), "flex items-center space-x-2", className)}
+    {...props}
+  >
+    <span className={`w-4 h-4 rounded-full border text-primary-foreground transition-all duration-200 flex items-center justify-center ${status == "checked" ? "bg-white border-white" : "border-border bg-primary-foreground"}`}>
+    {
+      status === "unchecked" ?
+        null
+      : status === "checked" ?
+        <CheckIcon className="h-4 w-4 " />
+      : status === "progress" ?
+        <span className="w-4 h-4 rounded-full border-b-2 border-b-primary animate-spin" /> : null
+    }
+    </span>
+    
+    
+    <span>
+      {props.children}
+    </span>
+  </div>
+))
+StatusIndicator.displayName = "StatusIndicator"
+
+export { StatusIndicator }
