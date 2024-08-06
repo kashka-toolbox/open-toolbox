83772f7e2c9de4e9e05b02880b87bb2c7dc07c8a
Morten Renner <github@mortenrenner.de>
2024-08-07 00:31:27 +0200

    add: StatusIndicators to Auth Debug page

--- src/app/[locale]/debug/auth/page.tsx
+++ src/app/[locale]/debug/auth/page.tsx
@@ -1,12 +1,22 @@
 "use client"
 
+import { AuthContext } from "@/components/auth-provider";
+import { StatusIndicator } from "@/components/ui/status-indicator";
+import { useContext } from "react";
+
 export default function Home() {
-  //const { isSignedIn } = useContext(AuthContext);
+  const { isSignedIn } = useContext(AuthContext) ?? {};
 
   return (
     <section className="flex flex-col gap-4">
       <span className="flex flex-col gap-1">
         <h1>Auth Test</h1>
+          <StatusIndicator status={isSignedIn ? "checked" : "unchecked"}>
+            is signed in
+          </StatusIndicator>
+          <StatusIndicator status={(localStorage.getItem("accessToken")?.length ?? 0) > 0 ? "checked" : "unchecked"}>
+            has access token
+          </StatusIndicator>
       </span>
     </section>
   );
