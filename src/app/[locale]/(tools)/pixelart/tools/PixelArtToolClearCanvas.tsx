516329fdcb6d261e2abd7cfba03654fbc3228ef6
Morten Renner <github@mortenrenner.de>
2024-10-06 19:58:12 +0200

    fix: build issues

--- src/app/[locale]/(tools)/pixelart/tools/PixelArtToolClearCanvas.tsx
+++ src/app/[locale]/(tools)/pixelart/tools/PixelArtToolClearCanvas.tsx
@@ -3,8 +3,14 @@ import { EraserIcon } from "@radix-ui/react-icons";
 import { CanvasTool } from "../CanvasTool";
 
 export class PixelArtToolClearCanvas extends CanvasTool {
+    onArtworkClick(x: number, y: number, mouseEvent: MouseEvent): void {
+        throw new Error("Method not implemented.");
+    }
+    onMouseMove(artworkX: number, artworkY: number, mouseEvent: MouseEvent, leftMouseDown: boolean): void {
+        throw new Error("Method not implemented.");
+    }
     constructor() {
-        super("Clear Canvas", <EraserIcon />);
+        super("Clear Canvas");
     }
 
     drawPreview(): void {
