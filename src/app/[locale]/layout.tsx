ef0b1a4b17cbbd46ccf626db9ddbd89a743303a5
Morten Renner <github@mortenrenner.de>
2024-10-19 19:34:30 +0200

    feat: pwa

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,6 +26,14 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
+
       <body className={cn("transition-colors", inter.className)}>
         <Providers locale={locale} messages={messages}>
           <Header />
---
+++ src/app/[locale]/offline/page.tsx
@@ -0,0 +1,10 @@
+export default function OfflinePage() {
+    return <div>
+        <h1 className="header-section-1">Offline</h1>
+        <p>
+            This page was not chached for offline use.
+            <br />
+            If you believe this is an error, please open an issue on our GitHub repository.
+        </p>
+    </div>
+}
