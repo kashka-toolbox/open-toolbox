f026369bdf10d37486627b24960c2c5aae2a8bcf
Morten Renner <github@mortenrenner.de>
2024-11-19 20:09:56 +0100

    fix: index fullscreen

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const t = useTranslations('home');
 
   return (
-    <>
+    <div className="container pl-1 pr-1 md:pl-4 md:pr-4">
       <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
         <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">{t('title')}</h1>
         <span className="max-w-[750px] text-center text-lg text-muted-foreground sm:text-xl">
@@ -102,6 +102,6 @@ export default function Home() {
           </AlertDescription>
         </Alert>
       </Section>
-    </>
+    </div>
   );
 }
