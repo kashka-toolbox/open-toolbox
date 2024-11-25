6de79697b15b4bcee1b4f4b7e89611e1093273df
Morten Renner <github@mortenrenner.de>
2024-11-25 23:11:42 +0100

    feat: add color display section with complementary and non-complementary styles

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -47,6 +47,34 @@ export default function Home() {
         </div>
       </Section>
 
+      <Section variant="ghost" className="mt-8">
+        <h1 className="header-section-1">Colors</h1>
+        All colors are defined in the theme file.
+
+        <h3 className="header-section-3">With complementary foreground color</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          <div className="bg-primary text-primary-foreground p-4 rounded-lg">Primary</div>
+          <div className="bg-secondary text-secondary-foreground p-4 rounded-lg">Secondary</div>
+          <div className="bg-destructive text-destructive-foreground p-4 rounded-lg">Destructive</div>
+          <div className="bg-muted text-muted-foreground p-4 rounded-lg">Muted</div>
+          <div className="bg-accent text-accent-foreground p-4 rounded-lg">Accent</div>
+          <div className="bg-popover text-popover-foreground p-4 rounded-lg">Popover</div>
+          <div className="bg-code text-code-foreground p-4 rounded-lg">Code</div>
+          <div className="bg-card text-card-foreground p-4 rounded-lg">Card</div>
+          <div className="bg-success text-success-foreground p-4 rounded-lg">Success</div>
+        </div>
+
+        <h3 className="header-section-3">Without complementary foreground color</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          <div className="bg-border text-foreground p-4 rounded-lg">border</div>
+          <div className="bg-input text-background p-4 rounded-lg">input</div>
+          <div className="bg-ring text-background p-4 rounded-lg">ring</div>
+          <div className="bg-background text-foreground p-4 rounded-lg">background</div>
+          <div className="bg-foreground text-background p-4 rounded-lg">foreground</div>
+          <div className="bg-border text-foreground p-4 rounded-lg">border</div>
+        </div>
+      </Section>
+
       <Section variant={"primary"} className="mt-8">
         <h1 className="header-section-1">Primary Section</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, tempore rem quidem incidunt repellat omnis obcaecati placeat a in explicabo nam, fuga accusantium odio atque hic doloribus. Quasi, ea molestiae.</p>
